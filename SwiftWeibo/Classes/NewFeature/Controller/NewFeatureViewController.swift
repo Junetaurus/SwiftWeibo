d28f945c123a5c446e878b994e291c95a9c40eeb
Taurus-GitHub <zj0376@163.com>
2017-03-16 15:57:41 +0800

    架构

---
+++ SwiftWeibo/Classes/NewFeature/Controller/NewFeatureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewFeatureViewController.swift
+//  SwiftWeibo
+//
+//  Created by ZhangJun on 2017/3/16.
+//  Copyright © 2017年 Taurus. All rights reserved.
+//
+
+import UIKit
+
+class NewFeatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ SwiftWeibo/Classes/Tool/Root/RootTool.swift
@@ -0,0 +1,13 @@
+//
+//  RootTool.swift
+//  SwiftWeibo
+//
+//  Created by ZhangJun on 2017/3/16.
+//  Copyright © 2017年 Taurus. All rights reserved.
+//
+
+import UIKit
+
+class RootTool: NSObject {
+
+}
