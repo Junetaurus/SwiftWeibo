1efe77a0e389332f4999646861072094b73bf718
Taurus-GitHub <zj0376@163.com>
2017-03-16 15:32:48 +0800

    first

---
+++ SwiftWeibo/AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  SwiftWeibo
+//
+//  Created by ZhangJun on 2017/3/16.
+//  Copyright © 2017年 Taurus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow.init()
+        self.window?.frame = UIScreen.main.bounds;
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        
+        let oAuthVC = OAuthViewController()
+        self.window?.rootViewController = oAuthVC
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+
+}
+
---
+++ SwiftWeibo/Classes/OAuth/Controller/OAuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OAuthViewController.swift
+//  SwiftWeibo
+//
+//  Created by ZhangJun on 2017/3/16.
+//  Copyright © 2017年 Taurus. All rights reserved.
+//
+
+import UIKit
+
+class OAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
