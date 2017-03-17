bbc2e7626c322d508bf393f42c9a5245adef4703
Taurus-GitHub <zj0376@163.com>
2017-03-17 16:11:05 +0800

    33

--- SwiftWeibo/Classes/Main/View/BadgeView.swift
+++ SwiftWeibo/Classes/Main/View/BadgeView.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
+let BadgeViewFont = UIFont.systemFont(ofSize: 11)
+
 class BadgeView: UIButton {
+    
+    var badgeValue: String? {
+        didSet {
+            if badgeValue?.isEmpty == true || badgeValue == nil{
+                self.isHidden = true;
+            } else {
+                if badgeValue! == "0" {
+                    self.isHidden = true;
+                } else {
+                    self.isHidden = false;
+                }
+                self.setTitle(badgeValue!, for: UIControlState.normal)
+            }
+            self.setBackgroundImage(UIImage.init(named: "main_badge"), for: UIControlState.normal)
+            self.setImage(nil, for: UIControlState.normal)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.isUserInteractionEnabled = false;
+        self.setBackgroundImage(UIImage.init(named: "main_badge"), for: UIControlState.normal)
+        self.titleLabel?.font = BadgeViewFont
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
 }
