1fc03c78997b26f278951da402c87397d801d360
Taurus-GitHub <zj0376@163.com>
2017-04-28 11:23:32 +0800

    接口

--- SwiftWeibo/Classes/Home/Controller/HomeViewController.swift
+++ SwiftWeibo/Classes/Home/Controller/HomeViewController.swift
@@ -13,6 +13,28 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.getTheLatestPublicWeibo(page: 1)
         // Do any additional setup after loading the view.
     }
+    
+    
+    //MARK:返回最新的公共微博
+    func getTheLatestPublicWeibo(page: NSInteger)  {
+        
+        if AccountTool.account().access_token == "" {
+            return
+        } else {
+            let params = NSMutableDictionary.init()
+            params["access_token"] = AccountTool.account().access_token
+            params["page"] = String(stringInterpolationSegment: page)
+            
+            HttpTool.shared.request(type: .GET, urlString: "https://api.weibo.com/2/statuses/public_timeline.json", paramters: params as? [String : Any]) { (result) in
+                
+            }
+        }
+    }
 }
+
+
+
+
